16c3de51b4c1e517a5ff8121050887723318578d
David Gould <davidgould@Davids-MacBook-Pro.local>
2020-10-22 17:21:48 -0500

    added service worker.js back in

--- server/server.js
+++ server/server.js
@@ -6,33 +6,18 @@ const bodyParser = require('body-parser');
 // Route includes
 const calculatorRouter = require('./routes/calculator.router');
 
-console.log('server1');
-
-
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log('server2');
-
-
 /* Routes */
 app.use('/calculator', calculatorRouter);
-
-console.log('server3');
-
 
 // Serve static files
 app.use(express.static('build'));
 
-console.log('server4');
-
-
 // App Set
 const PORT = process.env.PORT || 5000;
-
-console.log('server4');
-
 
 /** Listen * */
 app.listen(PORT, () => {
