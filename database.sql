0af5b2298a4b9356497b36f6091f0cd33b93b4a3
David Gould <davidgould@Davids-MacBook-Pro.local>
2020-10-22 01:08:31 -0500

    updated database.sql sql query to create table was missing id

--- database.sql
+++ database.sql
@@ -2,6 +2,7 @@
 
 -- Create table in database with sql query below
 CREATE TABLE "calculator_data" (
+"id" SERIAL PRIMARY KEY,
 "first_number" INT NOT NULL,
 "operator" VARCHAR (5) NOT NULL,
 "second_number" INT NOT NULL, 
