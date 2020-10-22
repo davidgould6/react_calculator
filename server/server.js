f26ad383c07a5bd21bc1b8a3a38971bf59c56b9c
David Gould <davidgould@Davids-MacBook-Pro.local>
2020-10-22 00:39:46 -0500

    base server create, listening

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, function () {
+  console.log('Listening on port: ', port);
+});
