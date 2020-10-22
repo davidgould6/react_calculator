c6d37e475764e3b644d90d21ce2fe0d792295a68
David Gould <davidgould@Davids-MacBook-Pro.local>
2020-10-22 00:51:02 -0500

    database.sql... Notes to create database and run query.

---
+++ database.sql
@@ -0,0 +1,8 @@
+-- Create database called "react_calculator"
+
+-- Create table in database with sql query below
+CREATE TABLE "calculator_data" (
+"first_number" INT NOT NULL,
+"operator" VARCHAR (5) NOT NULL,
+"second_number" INT NOT NULL, 
+"answer" INT NOT NULL);
